d967ee4bd6480cce20aa802f252652ea1e2381f2
minyoung22222 <alsdudr97@naver.com>
2022-11-13 16:14:04 +0900

    Chore: next 버전 다운그레이드

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Html } from 'next/document';
+import Head from 'next/head';
+
+export default function _document() {
+  return (
+    <Html>
+      <Head>
+        <title>Next App!</title>
+      </Head>
+    </Html>
+  );
+}
