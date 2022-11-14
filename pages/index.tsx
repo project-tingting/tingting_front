65a66f09a94c1149186994ea4492f0bfac7bd039
yoojs1205 <junsang8921@gmail.com>
2022-11-15 00:22:34 +0900

    #9 - Feat: Progress Bar 개발

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import UserInput from '../components/UserInput';
+import ProgressBar from '../components/ProgressBar';
 
 export default function index() {
   return (
     <>
       <div>
         <UserInput type="password" size="big" text="비밀번호" />
+        <ProgressBar />
       </div>
     </>
   );
