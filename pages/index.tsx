9489725ffc3347ae392a7e3d5a43fa386dacd84a
minyoung22222 <alsdudr97@naver.com>
2022-11-15 02:17:39 +0900

    충돌 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,8 @@ export default function index() {
     <>
       <div>
         <UserInput type="password" size="big" text="비밀번호" />
-<<<<<<< HEAD
         <ProgressBar />
-=======
         <Button text="계속하기" isRound={false} disabled={false}></Button>
->>>>>>> 30636b2048a82f63faca061f9376ed7936ed1554
       </div>
     </>
   );
