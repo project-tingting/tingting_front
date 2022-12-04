e573249b05eaab26442a72b09cf92435c9531ff7
minyoung22222 <alsdudr97@naver.com>
2022-12-04 16:41:07 +0900

    background blur 추가

--- components/Home/EditProfile.tsx
+++ components/Home/EditProfile.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import EditAlcohol from './EditAlcohol';
 import EditInterest from './EditInterest';
 import EditMbti from './EditMbti';
+import CloseButton from './CloseButton';
 
 export default function EditProfile() {
   const [tab, setTab] = useState(0);
@@ -13,26 +14,41 @@ export default function EditProfile() {
   };
 
   return (
-    <Wrapper>
-      <NavUl>
-        {editList.map((list, index) => (
-          <NavLi
-            key={list}
-            onClick={() => handleTab(index)}
-            className={tab === index ? 'selected' : ''}
-          >
-            {list}
-          </NavLi>
-        ))}
-      </NavUl>
-      {tab === 0 && <EditMbti />}
-      {tab === 1 && <EditAlcohol />}
-      {tab === 2 && <EditInterest />}
-    </Wrapper>
+    <BackGround>
+      <Wrapper>
+        <NavUl>
+          {editList.map((list, index) => (
+            <NavLi
+              key={list}
+              onClick={() => handleTab(index)}
+              className={tab === index ? 'selected' : ''}
+            >
+              {list}
+            </NavLi>
+          ))}
+        </NavUl>
+        {tab === 0 && <EditMbti />}
+        {tab === 1 && <EditAlcohol />}
+        {tab === 2 && <EditInterest />}
+        <ButtonContainer>
+          <CloseButton />
+        </ButtonContainer>
+      </Wrapper>
+    </BackGround>
   );
 }
 
+const BackGround = styled.div`
+  height: 100vh;
+  position: relative;
+  background: linear-gradient(180deg, #ffffff 50%, rgba(255, 255, 255, 0.5) 100%);
+  backdrop-filter: blur(5px);
+`;
+
 const Wrapper = styled.article`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
   background-color: ${({ theme }) => theme.colors.whiteColor};
   border-radius: 2rem 2rem 0 0;
   padding: 4.2rem 1.9rem;
@@ -57,4 +73,8 @@ const NavLi = styled.li`
     color: #161b27;
     border-bottom: 0.2rem solid ${({ theme }) => theme.colors.mainColor};
   }
+`;
+
+const ButtonContainer = styled.div`
+  text-align: center;
 `;
