ba28be0cfd78ab9a794037065ed43d4d04b2d631
minyoung22222 <alsdudr97@naver.com>
2022-12-09 17:40:46 +0900

    #23 - api 연동

--- core/api/baseInstance.ts
+++ core/api/baseInstance.ts
@@ -5,7 +5,7 @@ export const baseAPI = axios.create({
 });
 
 export const userAPI = axios.create({
-  baseURL: 'http://13.124.248.38:8080//tingting/user',
+  baseURL: 'http://52.79.235.50:8080/tingting/user',
   //   headers: {
   //     'Content-type': 'application/json',
   //   },
--- core/api/userprofileAPI.ts
+++ core/api/userprofileAPI.ts
@@ -3,7 +3,11 @@ import { baseAPI } from './baseInstance';
 export const userprofileAPI = async (userprofile: object[]) => {
   console.log(userprofile);
   try {
-    const res = await baseAPI.post('/userprofile', userprofile);
+    const res = await baseAPI.post('/userprofile', userprofile, {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('access-token'),
+      },
+    });
     return res.data;
   } catch (error) {
     console.error(error);
