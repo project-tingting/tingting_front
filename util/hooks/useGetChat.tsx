25a004c55f428be3a33a560c9c0fefc27f697ad9
promm <promm@daangn.com>
2022-12-20 05:21:38 +0900

    fix: 폴링적용

--- util/hooks/useGetChat.tsx
+++ util/hooks/useGetChat.tsx
@@ -7,6 +7,7 @@ export const useGetChat = (roomKey: string | string[] | undefined) => {
     return res.data;
   };
   const { data, refetch } = useQuery({
+    refetchInterval: 1000,
     queryKey: [`messageList/${roomKey}`],
     queryFn: () => {
       return getChat();
