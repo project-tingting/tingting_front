60d9cf015710cafb52f076fc9846c595de314511
minyoung22222 <alsdudr97@naver.com>
2022-12-20 16:53:06 +0900

    mainPic 변경

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 
-import mainPic from '../../public/assets/images/mainPic.jpg';
+import mainPic from '../../public/assets/images/mainPic.svg';
 
 export default function Banner() {
   return (
