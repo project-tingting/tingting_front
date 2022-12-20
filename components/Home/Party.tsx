7e9482e4ed82d346831d475f14403197c64c2c2d
YOOJS1205 <junsang8921@gmail.com>
2022-12-20 09:38:49 +0900

    Fix: 파일명 대소문자 구분

---
+++ components/Home/Party.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+
+import PartyModal from './PartyModal/PartyModal';
+
+import PartyIcon from '../../public/assets/icons/party.svg';
+// import DownArrowIcon from '../../public/assets/icons/arrow_down.svg';
+// import HostIcon from '../../public/assets/icons/host.svg';
+// import AddLogo from '../../public/assets/icons/add.svg';
+
+type IInnerScreen = {
+  isClicked: boolean;
+};
+
+export default function Party() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  // const handleClickPlusButton = () => {
+  //   setIsOpenModal(true);
+  // };
+
+  return (
+    <>
+      <Container>
+        <StyledContainer isClicked={isClicked}>
+          <GuideContainer>
+            <Image src={PartyIcon} alt="파티 아이콘" />
+            <PartyText>매칭 인원을 설정해주세요!</PartyText>
+          </GuideContainer>
+        </StyledContainer>
+        {/* {isClicked && (
+          <PartyContainer>
+            <Image src={HostIcon} alt="방장 로고" />
+            <AddButton onClick={handleClickPlusButton}>
+              <Image src={AddLogo} alt="추가 로고" />
+            </AddButton>
+          </PartyContainer>
+        )} */}
+      </Container>
+      {isOpenModal && <PartyModal setIsOpenModal={setIsOpenModal} />}
+    </>
+  );
+}
+
+const Container = styled.section`
+  position: relative;
+`;
+
+const StyledContainer = styled.section<IInnerScreen>`
+  background-color: ${({ theme }) => theme.colors.bgColor};
+  padding: 12px 24px;
+  padding: 0 24px;
+  display: flex;
+  justify-content: space-between;
+`;
+const GuideContainer = styled.section`
+  display: flex;
+  gap: 8px;
+`;
+
+const PartyText = styled.p`
+  font-size: 18px;
+  line-height: 56px;
+  color: ${({ theme }) => theme.colors.inputTextUserColor};
+`;
+
+const PartyContainer = styled.article`
+  border-top: 0.4px solid #adb3bc;
+  background-color: ${({ theme }) => theme.colors.whiteColor};
+  padding: 12px 24px;
+  position: absolute;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const AddButton = styled.button`
+  background: #d1c4f6;
+  border-radius: 50%;
+  padding: 12px;
+  width: 80px;
+  height: 80px;
+`;
---
+++ components/Home/TopNavigation.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+import { useUserLogout } from '../../core/apiHooks/user';
+
+import UserProfile from '../../public/assets/icons/UserProfile.svg';
+import Symbol from '../../public/assets/icons/symbol.svg';
+import Bell from '../../public/assets/icons/bell.svg';
+import No_Chat from '../../public/assets/icons/no_chat.svg';
+import outChat from '../../public/assets/icons/outChat.svg';
+import ChatModal from '../Chat/ChatModal';
+import Router, { useRouter } from 'next/router';
+
+interface TopNavProps {
+  isChat?: boolean;
+}
+
+export default function TopNavigation({ isChat }: TopNavProps) {
+  const { mutate: handleLogout } = useUserLogout();
+  const [modal, setModal] = useState(false);
+  const [isLogoClicked, setIsLogoClicked] = useState(false);
+  const router = useRouter();
+  const onClickBack = () => {
+    setModal(true);
+  };
+  const handleContinue = () => {
+    setModal(false);
+  };
+  const handleGoHome = () => {
+    router.back();
+  };
+  const handleGoChat = () => {
+    Router.push('/chat');
+  };
+  const handleClickLogo = () => {
+    setIsLogoClicked((prev) => !prev);
+  };
+  return (
+    <>
+      {modal && <ChatModal handleContinue={handleContinue} />}
+      <StyledContainer>
+        {isChat ? (
+          <Image src={outChat} alt="go back" onClick={onClickBack} />
+        ) : (
+          <Image src={UserProfile} alt="로고 버튼" onClick={handleClickLogo} />
+        )}
+        <Func>
+          <Image src={Bell} alt="알림 버튼" />
+          {isChat ? (
+            <Image src={Symbol} alt="로고 버튼" onClick={handleGoHome} />
+          ) : (
+            <Image src={No_Chat} alt="채팅 알림 버튼" onClick={handleGoChat} />
+          )}
+        </Func>
+      </StyledContainer>
+      {isLogoClicked && (
+        <LogoutButton onClick={() => handleLogout(localStorage.getItem('access-token'))}>
+          로그아웃
+        </LogoutButton>
+      )}
+    </>
+  );
+}
+
+const StyledContainer = styled.article`
+  background: linear-gradient(180deg, #ffffff 50%, rgba(255, 255, 255, 0.5) 100%);
+  height: 4.4rem;
+  padding: 0 1.6rem;
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+`;
+
+const Func = styled.section`
+  display: flex;
+  gap: 1.6rem;
+`;
+
+const LogoutButton = styled.button`
+  position: absolute;
+  left: 1.6rem;
+  z-index: 100;
+  background-color: #f2f2f2;
+  border-radius: 1rem;
+  font-size: 2rem;
+  line-height: 2.4rem;
+  padding: 0.8rem 2rem;
+`;
