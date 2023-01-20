432be75cc9631e684888e1aabe6d3885abe8a334
YOOJS1205 <junsang8921@gmail.com>
2023-01-20 15:13:28 +0900

    Refactor: 환경 변수 세팅 및 로그인 로그아웃 로직 react-query로 변경

--- core/api/user.ts
+++ core/api/user.ts
@@ -1,42 +1,17 @@
-import Router from 'next/router';
-import axios from 'axios';
 import { userAPI } from './baseInstance';
-
-const loginNGoMain = (accessToken: string, uuid: string) => {
-  localStorage.setItem('access-token', accessToken);
-  Router.push(`/home/${uuid}`);
-};
-
-const logoutNGoToMain = () => {
-  Router.push('/');
-};
 
 // 로그인은 개발 중
 export const submitLogin = async (userId: string, password: string) => {
-  try {
-    const res = await userAPI.post('/login', {
-      userId: userId,
-      password: password,
-    });
-    loginNGoMain(res.data.data.accessToken, res.data.data.uuid);
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      return error.response?.status;
-    }
-  }
+  return await userAPI.post('/login', {
+    userId: userId,
+    password: password,
+  });
 };
 
 export const submitLogout = async (accessToken: string | null) => {
-  try {
-    localStorage.clear();
-    const res = await userAPI.post('/logout', {
-      accessToken: accessToken,
-    });
-    logoutNGoToMain();
-    return res;
-  } catch (error) {
-    console.error(error);
-  }
+  return await userAPI.post('/logout', {
+    accessToken: accessToken,
+  });
 };
 
 export const getUserInfo = async () => {
--- core/apiHooks/user.ts
+++ core/apiHooks/user.ts
@@ -1,5 +1,5 @@
+import Router from 'next/router';
 import { useMutation, useQuery } from '@tanstack/react-query';
-
 import { submitLogin, submitLogout, getUserInfo } from '../api/user';
 
 type userInfo = {
@@ -9,11 +9,27 @@ type userInfo = {
 
 // 개발 중
 export const useUserLogin = ({ userId, password }: userInfo) => {
-  return useMutation(() => submitLogin(userId, password));
+  return useMutation(() => submitLogin(userId, password), {
+    onSuccess: (data) => {
+      localStorage.setItem('access-token', data.data.accessToken);
+      Router.push(`/home/${data.data.uuid}`);
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
 };
 
 export const useUserLogout = () => {
-  return useMutation((accessToken: string | null) => submitLogout(accessToken));
+  return useMutation((accessToken: string | null) => submitLogout(accessToken), {
+    onSuccess: () => {
+      localStorage.clear();
+      Router.push('/');
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
 };
 
 export const useGetUserInfo = () => {
