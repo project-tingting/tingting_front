9e668a983a12ee39106210c1ed26cb818d094935
YOOJS1205 <junsang8921@gmail.com>
2023-02-15 21:31:19 +0900

    Refactor: 서브 텍스트 추가

--- components/common/Guide.tsx
+++ components/common/Guide.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 type Props = {
   text: string;
   isCenter?: boolean;
+  subText?: string;
 };
 
 type TextProps = {
   isCenter?: boolean;
 };
 
-export default function Guide({ text, isCenter }: Props) {
+export default function Guide({ text, isCenter, subText }: Props) {
   return (
     <>
       <GuideText isCenter={isCenter}>{text}</GuideText>
+      {subText && <SubGuide>{subText}</SubGuide>}
     </>
   );
 }
@@ -25,4 +27,17 @@ const GuideText = styled.h1<TextProps>`
   padding: 5rem 0 4.5rem;
 
   text-align: ${({ isCenter }) => isCenter && 'center'};
+`;
+
+const SubGuide = styled.p<TextProps>`
+  color: ${({ theme }) => theme.colors.descriptionColor};
+  font-weight: 500;
+  font-size: 2rem;
+  line-height: 2.4rem;
+  margin-top: -3.8rem;
+  ${({ isCenter }) =>
+    isCenter &&
+    css`
+      text-align: center;
+    `}
 `;
